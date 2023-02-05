6070bb7a29e44a0a6a3c893b4b221a569066605e
Jed Duffey <jedduffey@gmail.com>
2023-02-05 13:24:08 -0500

    Change chain id to Mumbai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,14 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+    return (
+        <ThirdwebProvider desiredChainId={activeChainId}>
+            <Component {...pageProps} />
+        </ThirdwebProvider>
+    );
 }
 
 export default MyApp;
