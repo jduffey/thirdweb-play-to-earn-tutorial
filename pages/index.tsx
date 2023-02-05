6ab1c731287222a3becb5554b1431a98402c74ec
Jed Duffey <jedduffey@gmail.com>
2023-02-05 13:39:52 -0500

    Code for index.tsx; create/replace styles for Home and Gameplay

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,76 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import {
+    ConnectWallet,
+    useAddress,
+    useEditionDrop,
+    useOwnedNFTs,
+} from "@thirdweb-dev/react";
+import { CHARACTERS_ADDRESS } from "../const/contractAddresses";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+    const editionDrop = useEditionDrop(CHARACTERS_ADDRESS);
+
+    const address = useAddress();
+
+    const router = useRouter();
+
+    const {
+        data: ownedNFTs,
+        isLoading,
+        isError,
+    } = useOwnedNFTs(editionDrop, address);
+
+    // 0. Wallet Connect - required to check if they own an NFT
+    if (!address) {
+        return (
+            <div className={styles.container}>
+                <ConnectWallet />
+            </div>
+        );
+    }
+
+    // 1. Loading
+    if (isLoading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    }
+
+    // If something went wrong..
+    if (!ownedNFTs || isError) {
+        return (
+            <div>
+                Error
+            </div>
+        );
+    }
+
+    // 2. If they don't own an NFT, show them the mint page
+    if (ownedNFTs.length === 0) {
+        return (
+            <div className={styles.container}>
+                <MintContainer />
+            </div>
+        );
+    }
+
+    // 3. If they own an NFT, show them the NFT page
     return (
         <div className={styles.container}>
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-                </h1>
-
-                <p className={styles.description}>
-                    Get started by configuring your desired network in{" "}
-                    <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-                    <code className={styles.code}>pages/index.tsx</code> file!
-                </p>
-
-                <div className={styles.connect}>
-                    <ConnectWallet accentColor="#f213a4" colorMode="light" />
-                </div>
-
-                <div className={styles.grid}>
-                    <a href="https://portal.thirdweb.com/" className={styles.card}>
-                        <h2>Portal &rarr;</h2>
-                        <p>
-                            Guides, references and resources that will help you build with
-                            thirdweb.
-                        </p>
-                    </a>
-
-                    <a href="https://thirdweb.com/dashboard" className={styles.card}>
-                        <h2>Dashboard &rarr;</h2>
-                        <p>
-                            Deploy, configure and manage your smart contracts from the
-                            dashboard.
-                        </p>
-                    </a>
-
-                    <a
-                        href="https://portal.thirdweb.com/templates"
-                        className={styles.card}
-                    >
-                        <h2>Templates &rarr;</h2>
-                        <p>
-                            Discover and clone template projects showcasing thirdweb features.
-                        </p>
-                    </a>
-                </div>
-            </main>
+            <button
+                className={`${styles.mainButton} ${styles.spacerBottom}`}
+                onClick={() => router.push('/play')}
+            >
+                Play Game
+            </button>
         </div>
     );
-};
+
+
+}
 
 export default Home;
